5960b5474ff54c5b66b4f308d848b185cf119547
Pavel Drankov <titantins@gmail.com>
2018-08-05 22:49:23 +0300

    improving build script

--- .travis.sh
+++ .travis.sh
@@ -4,4 +4,5 @@ git clone https://github.com/Sammers21/vertx-cassandra-client
 cd vertx-cassandra-client
 git checkout with_codegen
 mvn clean install -Dmaven.test.skip=true -s .travis.maven.settings.xml
-gradle build
+cd ..
+./gradlew build
