6d5c232193b9bee61a165b5349e5a2e3f2b484da
Pavel Drankov <titantins@gmail.com>
2018-08-05 22:37:31 +0300

    add travis build

---
+++ .travis.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+
+git clone https://github.com/Sammers21/vertx-cassandra-client
+cd vertx-cassandra-client
+git checkout with_codegen
+mvn clean install -Dmaven.test.skip=true -s .travis.maven.settings.xml
+gradle build
