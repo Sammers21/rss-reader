1407efabc3bf10a867af69647384ba156781cae1
Sammers21 <titantins@gmail.com>
2018-11-10 19:55:38 +0300

    remove manual cassandra client build

--- .travis.sh
+++ .travis.sh
@@ -1,8 +1,3 @@
 #!/bin/bash
 
-git clone https://github.com/Sammers21/vertx-cassandra-client
-cd vertx-cassandra-client
-git checkout with_codegen
-mvn clean install -Dmaven.test.skip=true -s .travis.maven.settings.xml
-cd ..
 ./gradlew build
